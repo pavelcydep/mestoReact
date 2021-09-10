74cbde4858ef234ebe30ca02f6e2cf8ae5168d66
pavel <pavel/ps2014@yandex.ru>
2021-09-10 21:25:06 +0300

    Добавил проект

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useEffect, useState } from 'react';
+import UserData from './components/UserData';
+import OnLoadingUserData from './components/OnLoadingUserData';
+import axios from 'axios';
+import { PopupEdit } from './components/PopupEdit';
+import PopupAdd from './components/PopupAdd';
+import { BrowserRouter as Router} from 'react-router-dom';
+import { Route,Switch } from 'react-router-dom';
+import { Step1 } from './components/PopupEdit';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import PlacesList from './components/PlacesList';
 function App() {
+  const [isEditProfilePopupOpen, setPopupProfile] = React.useState(false);
+  const onEditProfile = () => setPopupProfile(!isEditProfilePopupOpen);
+
+  const onCloseAll = () => {
+    setPopupProfile(false);}
+
+
+
+  
+
+
+
+   
+     
+      
+   
+
+
+
+
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header/>
+     <Profile
+      onClickEdit={onEditProfile} >
+ 
+
+
+
+      </Profile>
+      
+
+  
+        <PlacesList/>
+        
+        
+        <PopupEdit 
+        isOpen={isEditProfilePopupOpen}
+         onClose={onCloseAll}
+        />
+       
+      <PopupAdd  
+        //isOpenAdd={isAddPopupOpen}
+         //onClose={onCloseAll}
+        />
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './App.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DataProvider } from './DataContext';
 
 ReactDOM.render(
   <React.StrictMode>
+    <DataProvider>
     <App />
+    </DataProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
